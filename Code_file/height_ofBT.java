b56463d58f278c885c3c7b01fedb792c186f731c
karan jagota <karanjagota@users.noreply.github.com>
2017-06-04 20:32:56 +0530

    Add files via upload

---
+++ Code_file/height_ofBT.java
@@ -0,0 +1,15 @@
+
+// height of tree in java .. only function 
+int height(Node root){
+    int leftHeight = 0;
+    int rightHeight = 0;
+    if (root != null){
+        if (root.left != null){
+            leftHeight = height(root.left);
+        }
+        if (root.right != null){
+            rightHeight = height(root.right);
+        }
+    }
+    return leftHeight > rightHeight ? leftHeight+1 : rightHeight+1;
+}
